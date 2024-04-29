f6d54da5bd1a0852b30d0dbaf27484fc11585202
ArfanMThafseer <90204858+PopieGloria@users.noreply.github.com>
2024-04-29 22:53:47 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,4 @@
-<!-- /*!
-* Author Name: MH RONY.
-* GigHub Link: https://github.com/dev-mhrony
-* Facebook Link:https://www.facebook.com/dev.mhrony
-* Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com
-* Visit My Website : developerrony.com
-*/ -->
+
 <?php
 $servername = "localhost";
 $username = "root";
@@ -18,11 +11,3 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 // Die if connection was not successful
 ?>
 
-<!-- /*!
-* Author Name: MH RONY.
-* GigHub Link: https://github.com/dev-mhrony
-* Facebook Link:https://www.facebook.com/dev.mhrony
-* Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com
-* Visit My Website : developerrony.com
-*/ -->
